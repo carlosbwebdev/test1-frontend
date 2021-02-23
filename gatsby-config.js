f75e1f9ba26f1741ef8d51f0cd5f82441d7de41f
carlosbwebdev <carlosbwebdev@gmail.com>
2021-02-23 14:37:08 +0000

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,32 @@
 module.exports = {
   siteMetadata: {
-    title: "frontend",
+    title: "Basic Template Starter",
+    author: "@CarlosBarros",
+    contact: "carlosbwebdev@gmail.com",
   },
-  plugins: [],
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `assets`,
+        path: `${__dirname}/src/assets/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: `http://localhost:1337`,
+        queryLimit: 1000, // Default to 100
+        //   contentTypes : `jobs`, `projects`, `blogs`,
+        //   singleType : `about`
+        //  ONLY ADD TO ARRAY IF YOU HAVE DATA IN STRAPI !!!!
+        contentTypes: [],
+        singleTypes: [],
+      },
+    },
+  ],
 };
---
+++ src/components/layout.js
@@ -0,0 +1,9 @@
+import React from "react";
+// import Navbar from "./Navbar";
+// import Sidebar from "./Sidebar";
+// import Footer from "./Footer";
+const Layout = ({ children }) => {
+  return <>{children}</>;
+};
+
+export default Layout;
