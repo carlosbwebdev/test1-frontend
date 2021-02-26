9c3a0c46a6b60b8eaf7ec308356e4b5b70a4235e
carlosbwebdev <carlosbwebdev@gmail.com>
2021-02-26 19:04:51 +0000

    add footer to layout and add toggle button func

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "../css/layout.css";
 import Navbar from "./navbar";
+import Sidebar from "../components/sidebar";
+import Footer from "../components/footer";
+
 const Layout = ({ children }) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
-      <Navbar />
+      <Navbar toggleSidebar={toggleSidebar} />
+      <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
       {children}
+      <Footer />
     </>
   );
 };
