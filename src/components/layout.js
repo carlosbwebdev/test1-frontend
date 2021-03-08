767b064c9cdfbdc4e82e821ff8e3ddbfde91d7d1
carlosbwebdev <carlosbwebdev@gmail.com>
2021-03-08 07:56:09 +0000

    add icon

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,13 @@ import Sidebar from "../components/sidebar";
 import Footer from "../components/footer";
 import ReactDOM from "react-dom";
 import disableScroll from "disable-scroll";
+import favicon from "../assets/dC.ico";
+import Helmet from "react-helmet";
 
 const Layout = ({ children }) => {
   const [isOpen, setIsOpen] = React.useState(false);
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
-
     if (!isOpen) {
       disableScroll.on();
     }
@@ -18,6 +19,9 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <Helmet>
+        <link rel="icon" href={favicon} />
+      </Helmet>
       <Navbar toggleSidebar={toggleSidebar} />
       <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
       {children}
