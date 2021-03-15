06c496c7c93a8ca1300cf651a2ad8ae84f8bd32a
carlosbwebdev <carlosbwebdev@gmail.com>
2021-03-15 09:01:15 +0000

    try to fix error in  building process

--- src/components/darkMode.js
+++ src/components/darkMode.js
@@ -27,44 +27,46 @@ import React from "react";
 import "../css/DarkMode.css";
 
 const DarkMode = () => {
-  let clickedClass = "clicked";
-  const body = window.body;
-  const lightTheme = "light";
-  const darkTheme = "dark";
-  let theme;
+  if (typeof window !== `undefined`) {
+    let clickedClass = "clicked";
+    const body = document.body;
+    const lightTheme = "light";
+    const darkTheme = "dark";
+    let theme;
 
-  if (localStorage) {
-    theme = localStorage.getItem("theme");
-  }
-
-  if (theme === lightTheme || theme === darkTheme) {
-    body.classList.add(theme);
-  } else {
-    body.classList.add(lightTheme);
-  }
-
-  const switchTheme = (e) => {
-    if (theme === darkTheme) {
-      body.classList.replace(darkTheme, lightTheme);
-      e.target.classList.remove(clickedClass);
-      localStorage.setItem("theme", "light");
-      theme = lightTheme;
-    } else {
-      body.classList.replace(lightTheme, darkTheme);
-      e.target.classList.add(clickedClass);
-      localStorage.setItem("theme", "dark");
-      theme = darkTheme;
+    if (localStorage) {
+      theme = localStorage.getItem("theme");
     }
-  };
 
-  return (
-    <button
-      className={theme === "dark" ? clickedClass : ""}
-      id="darkMode"
-      className={"btnDarkMode"}
-      onClick={(e) => switchTheme(e)}
-    ></button>
-  );
+    if (theme === lightTheme || theme === darkTheme) {
+      body.classList.add(theme);
+    } else {
+      body.classList.add(lightTheme);
+    }
+
+    const switchTheme = (e) => {
+      if (theme === darkTheme) {
+        body.classList.replace(darkTheme, lightTheme);
+        e.target.classList.remove(clickedClass);
+        localStorage.setItem("theme", "light");
+        theme = lightTheme;
+      } else {
+        body.classList.replace(lightTheme, darkTheme);
+        e.target.classList.add(clickedClass);
+        localStorage.setItem("theme", "dark");
+        theme = darkTheme;
+      }
+    };
+
+    return (
+      <button
+        className={theme === "dark" ? clickedClass : ""}
+        id="darkMode"
+        className={"btnDarkMode"}
+        onClick={(e) => switchTheme(e)}
+      ></button>
+    );
+  }
 };
 
 export default DarkMode;
