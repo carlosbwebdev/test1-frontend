a1f6da3e48f7cda48e6d27b03923e0421440c84b
carlosbwebdev <carlosbwebdev@gmail.com>
2021-03-16 21:27:41 +0000

    add title component

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -3,22 +3,25 @@ import Layout from "../components/layout";
 import { graphql, Link } from "gatsby";
 import styles from "../css/blog-template.module.css";
 import ReactMarkdown from "react-markdown";
+import Title from "../components/title";
 
 export const query = graphql`
   query GetSingleBlog($slug: String) {
     blog: strapiBlogs(slug: { eq: $slug }) {
       content
+      title
     }
   }
 `;
 
 const BlogTemplate = ({ data }) => {
-  const { content } = data.blog;
+  const { content, title } = data.blog;
 
   return (
     <Layout>
       <section className={styles.blogTemplate} id="blogTemplateDark">
         <div className={styles.sectionCenter}>
+          <Title title={title} className={styles.title} />
           <article className={styles.blogContent}>
             <ReactMarkdown source={content} />
           </article>
