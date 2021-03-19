9aed377abfa91c52c4a9c48213b240e67c21b0a9
carlosbwebdev <carlosbwebdev@gmail.com>
2021-03-19 07:09:47 +0000

    create real content

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -20,8 +20,9 @@ const BlogTemplate = ({ data }) => {
   return (
     <Layout>
       <section className={styles.blogTemplate} id="blogTemplateDark">
+        <Title title={title} className={styles.title} />
+        <div className={styles.underline}></div>
         <div className={styles.sectionCenter}>
-          <Title title={title} className={styles.title} />
           <article className={styles.blogContent}>
             <ReactMarkdown source={content} />
           </article>
