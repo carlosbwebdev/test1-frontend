eb9d5d0dccb4f65e72c18cf965cb97d06edf2bab
carlosbwebdev <carlosbwebdev@gmail.com>
2021-03-19 12:27:15 +0000

    add picture

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -35,7 +35,10 @@ const SEO = ({ title, description }) => {
       <meta name="twitter:creactor" content={twitterUsername} />
       <meta name="twitter:title" content={siteTitle} />
       <meta name="twitter:description" content={siteDesc} />
-      <meta name="twitter:image" content={`${siteUrl}${image}`} />
+      <meta
+        name="twitter:image"
+        content="https://res.cloudinary.com/doge7vn6i/image/upload/v1616156760/twitter-card_tadkg7.png"
+      />
     </Helmet>
   );
 };
